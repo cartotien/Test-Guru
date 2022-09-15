6c9eb9c19684535eebd7cd6e3f9d40ec8d5802aa
Dmitry Lyutsko <dmitry.lyutsko@yandex.ru>
2022-09-15 19:07:21 +0300

    Dropped redundant self in methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,8 @@
 class Test < ApplicationRecord
   def self.by_category(category)
-    self.joins('JOIN categories ON tests.category_id = categories.id')
-        .where('categories.title LIKE ?', category)
-        .order(title: :desc)
-        .pluck(:title)
+    joins('JOIN categories ON tests.category_id = categories.id')
+      .where('categories.title LIKE ?', category)
+      .order(title: :desc)
+      .pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   def user_tests(level)
     Test.joins('JOIN users ON tests.author_id = users.id')
-        .where('author_id = ? AND level = ?', self.id, level)
+        .where('author_id = ? AND level = ?', id, level)
   end
 end
