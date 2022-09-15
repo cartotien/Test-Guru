17623dbc723246ec7986422989cb5ec0a998a6a0
Dmitry Lyutsko <dmitry.lyutsko@yandex.ru>
2022-09-15 18:54:33 +0300

    Added model methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,8 @@
 class Test < ApplicationRecord
+  def self.by_category(category)
+    self.joins('JOIN categories ON tests.category_id = categories.id')
+        .where('categories.title LIKE ?', category)
+        .order(:desc)
+        .pluck(:title)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  def user_tests(level)
+    Test.joins('JOIN users ON tests.author_id = users.id')
+        .where('author_id = ? AND level = ?', self.id, level)
+  end
 end
