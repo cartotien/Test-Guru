2ecab2ec6c9da957d34d2fa4aef45f5d50d59fbd
Dmitry Lyutsko <dmitry.lyutsko@yandex.ru>
2022-09-20 09:15:44 +0300

    Resolved issues

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,13 +1,14 @@
 class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: 'User'
-  has_many :questions
-  has_many :test_passages
+
+  has_many :questions, dependent: :destroy
+  has_many :test_passages, dependent: :destroy
   has_many :users, through: :test_passages
 
-  def self.by_category(category)
-    joins('JOIN categories ON tests.category_id = categories.id')
-      .where('categories.title LIKE ?', category)
+  def self.by_category(category_title)
+    joins(:category)
+      .where(categories: { title: category_title })
       .order(title: :desc)
       .pluck(:title)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :authored_tests, class_name: 'Test', foreign_key: 'author_id'
-  has_many :test_passages
+  has_many :authored_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
+  has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
 
   def user_tests(level)
