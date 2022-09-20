149e9a0dca571c07cdac756ec1f7c54a51f71bb2
Dmitry Lyutsko <dmitry.lyutsko@yandex.ru>
2022-09-21 01:05:44 +0300

    Added answers amount validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,14 @@
 class Answer < ApplicationRecord
   validates :body, presence: true
+  validate :cannot_have_more_than_four_answers, on: :create
 
   belongs_to :question
 
   scope :correct, -> { where(correct: true) }
+
+  protected
+
+  def cannot_have_more_than_four_answers
+    errors.add(:question, "can't have more than 4 answers") if question.answers.count >= 4
+  end
 end
