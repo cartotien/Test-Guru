21a572659ec84197fb5ff6f2039a89cb4f6f3ee1
Dmitry Lyutsko <dmitry.lyutsko@yandex.ru>
2022-09-22 19:14:40 +0300

    Created controllers and views for questions

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,44 @@
+class QuestionsController < ApplicationController
+  before_action :find_test, only: %i[index create]
+  before_action :find_question, only: %i[show destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
+  def index
+    @questions = @test.questions
+    render inline: '<li><% @questions.each do |q| %><p><%= q.body %></p><% end %></li>'
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+    @test.questions.create!(question_params)
+    redirect_to test_questions_path
+  end
+
+  def destroy
+    @question.destroy
+    render plain: "Question successfully deleted"
+  end
+
+  protected
+
+  def question_params
+    params.require(:question).permit(:body)
+  end
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def rescue_with_question_not_found
+    render plain: "Question not found."
+  end
+end
