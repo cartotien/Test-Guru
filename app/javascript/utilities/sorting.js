7eeb01bd85c1cd077ba0fef0322e87c429d3acee
Dmitry Lyutsko <dmitry.lyutsko@yandex.ru>
2022-10-12 10:35:05 +0300

    OOP Style sorting refactor

--- app/javascript/utilities/sorting.js
+++ app/javascript/utilities/sorting.js
@@ -1,56 +1,103 @@
+// document.addEventListener('turbolinks:load', function() {
+//   var control = document.querySelector('.sort-by-title')
+
+//   if (control) { control.addEventListener('click', sortRowsByTitle) }
+// })
+
+// function sortRowsByTitle() {
+//
+
+//   for (var i = 1; i < rows.length; i++) {
+//     sortedRows.push(rows[i])
+//   }
+
+//   if (this.querySelector('.octicon-arrow-up').classList.contains('hide')) {
+//     sortedRows.sort(compareRowsAsc)
+//     this.querySelector('.octicon-arrow-up').classList.remove('hide')
+//     this.querySelector('.octicon-arrow-down').classList.add('hide')
+//   } else {
+//     sortedRows.sort(compareRowsDesc)
+//     this.querySelector('.octicon-arrow-down').classList.remove('hide')
+//     this.querySelector('.octicon-arrow-up').classList.add('hide')
+//   }
+
+//   var sortedTable = document.createElement('table')
+
+//   sortedTable.classList.add('table', 'table-primary', 'table-striped')
+//   sortedTable.appendChild(rows[0])
+
+//   for (var i = 0; i < sortedRows.length; i++) {
+//     sortedTable.appendChild(sortedRows[i])
+//   }
+
+//   table.parentNode.replaceChild(sortedTable, table)
+
+
+class RowSort {
+  constructor() {
+    this.control = document.querySelector('.sort-by-title')
+    this.setup()
+  }
+
+  setup(){
+    this.control.addEventListener('click', event => { this.sortRows() })
+  }
+
+
+  sortRows() {
+    let table = document.querySelector('table')
+
+    let rows = table.querySelectorAll('tr')
+    let sortedRows = []
+
+
+    for (let i = 1; i < rows.length; i++) {
+      sortedRows.push(rows[i])
+    }
+  
+    if (this.control.querySelector('.octicon-arrow-up').classList.contains('hide')) {
+      sortedRows.sort(this.compareRowsAsc)
+      this.control.querySelector('.octicon-arrow-up').classList.remove('hide')
+      this.control.querySelector('.octicon-arrow-down').classList.add('hide')
+    } else {
+      sortedRows.sort(this.compareRowsDesc)
+      this.control.querySelector('.octicon-arrow-down').classList.remove('hide')
+      this.control.querySelector('.octicon-arrow-up').classList.add('hide')
+    }
+  
+    let sortedTable = document.createElement('table')
+  
+    sortedTable.classList.add('table', 'table-primary', 'table-striped')
+    sortedTable.appendChild(rows[0])
+  
+    for (let i = 0; i < sortedRows.length; i++) {
+      sortedTable.appendChild(sortedRows[i])
+    }
+  
+    table.parentNode.replaceChild(sortedTable, table)
+  }
+
+  compareRowsAsc(row1, row2) {
+    let testTitle1 = row1.querySelector('td').textContent
+    let testTitle2 = row2.querySelector('td').textContent
+    
+    if (testTitle1 > testTitle2) { return 1 }
+    if (testTitle1 < testTitle2) { return -1 }
+    return 0 
+  }
+  
+  compareRowsDesc(row1, row2) {
+    let testTitle1 = row1.querySelector('td').textContent
+    let testTitle2 = row2.querySelector('td').textContent
+    
+    if (testTitle1 > testTitle2) { return -1 }
+    if (testTitle1 < testTitle2) { return 1 }
+    return 0 
+  }
+}
+
 document.addEventListener('turbolinks:load', function() {
-  var control = document.querySelector('.sort-by-title')
-
-  if (control) { control.addEventListener('click', sortRowsByTitle) }
-})
-
-function sortRowsByTitle() {
-  var table = document.querySelector('table')
-
-  var rows = table.querySelectorAll('tr')
-  var sortedRows = []
-
-  for (var i = 1; i < rows.length; i++) {
-    sortedRows.push(rows[i])
-  }
-
-  if (this.querySelector('.octicon-arrow-up').classList.contains('hide')) {
-    sortedRows.sort(compareRowsAsc)
-    this.querySelector('.octicon-arrow-up').classList.remove('hide')
-    this.querySelector('.octicon-arrow-down').classList.add('hide')
-  } else {
-    sortedRows.sort(compareRowsDesc)
-    this.querySelector('.octicon-arrow-down').classList.remove('hide')
-    this.querySelector('.octicon-arrow-up').classList.add('hide')
-  }
-
-  var sortedTable = document.createElement('table')
-
-  sortedTable.classList.add('table', 'table-primary', 'table-striped')
-  sortedTable.appendChild(rows[0])
-
-  for (var i = 0; i < sortedRows.length; i++) {
-    sortedTable.appendChild(sortedRows[i])
-  }
-
-  table.parentNode.replaceChild(sortedTable, table)
-
-}
-
-function compareRowsAsc(row1, row2) {
-  var testTitle1 = row1.querySelector('td').textContent
-  var testTitle2 = row2.querySelector('td').textContent
+    const control = document.querySelector('.sort-by-title')
   
-  if (testTitle1 > testTitle2) { return 1 }
-  if (testTitle1 < testTitle2) { return -1 }
-  return 0 
-}
-
-function compareRowsDesc(row1, row2) {
-  var testTitle1 = row1.querySelector('td').textContent
-  var testTitle2 = row2.querySelector('td').textContent
-  
-  if (testTitle1 > testTitle2) { return -1 }
-  if (testTitle1 < testTitle2) { return 1 }
-  return 0 
-}
+    if (control) new RowSort
+  })
