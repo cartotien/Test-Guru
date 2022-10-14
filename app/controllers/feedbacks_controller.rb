ce1cb3929ac535e3ae24e3f58c85a209fa10c045
Dmitry Lyutsko <dmitry.lyutsko@yandex.ru>
2022-10-14 14:39:51 +0300

    Created feedback form

---
+++ app/controllers/feedbacks_controller.rb
@@ -0,0 +1,20 @@
+class FeedbacksController < ApplicationController
+  def new
+    @feedback = current_user.feedbacks.build
+  end
+
+  def create
+    @feedback = current_user.feedbacks.build(feedback_params)
+    if @feedback.save
+      redirect_to root_path, notice: t('.success')
+    else
+      render :new
+    end
+  end
+
+  protected
+
+  def feedback_params
+    params.require(:feedback).permit(:title, :body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :users, only: :create
   resources :sessions, only: :create
+  resources :feedbacks, only: %i[new create]
 
   resources :tests do
     resources :questions, shallow: true, except: :index do
